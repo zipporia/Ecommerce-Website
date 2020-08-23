f6e8cba680847284bd56746b85084454249343d6
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-23 20:46:32 +0800

    Models
    Order Class created

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -18,3 +18,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
