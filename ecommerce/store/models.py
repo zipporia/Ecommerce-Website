031d810efcc7241f04d1e479e26ef1949cd96e21
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-29 18:55:25 +0800

    shipping address added

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)  # one to one meaning the user can only have one customer and the customer can only have on user
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return self.name
+        return self.name  # this is going to be the value that show up in our admin panel
 
 
 class Product(models.Model):
@@ -36,3 +36,15 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=False)
     date_added = models.DateTimeField(auto_now_add=True)
 
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
