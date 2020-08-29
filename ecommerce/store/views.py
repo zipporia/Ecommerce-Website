d89bdd9adfb016411706348a3595d90929f6ff13
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-29 23:37:37 +0800

    item added to the cart

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -9,10 +9,17 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
+
