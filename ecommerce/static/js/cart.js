69762afcf81ed4c284f0ea3123e3f4625ab37a53
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-30 19:48:04 +0800

    Json response

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -5,5 +5,14 @@ for(var i = 0; i < updateBtns.length; i++){
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:', user)
+
+        if(user == 'AnonymousUser'){
+            console.log('Not Logged in')
+        }else{
+            console.log('User is logged in, sending data..')
+        }
     })
+
 }
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 
@@ -31,3 +32,6 @@ def checkout(request):
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
 
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
