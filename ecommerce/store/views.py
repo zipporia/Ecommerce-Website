90b90682c602606b36b7a8086f744f38dc5ed300
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-05 19:16:24 +0800

    view update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -41,4 +41,10 @@ def updateItem(request):
 
     print('Action:', action)
     print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
     return JsonResponse('Item was added', safe=False)
