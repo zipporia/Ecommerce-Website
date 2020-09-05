ae4cc8a79b6c539cb1f641cb9fbc8dbcf452fd97
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-05 20:03:24 +0800

    views update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,8 +5,18 @@ import json
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
 
@@ -15,6 +25,7 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
