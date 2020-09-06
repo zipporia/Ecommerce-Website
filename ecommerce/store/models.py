277a3805c0151c223e34d264f7890cf05cf2603b
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-06 10:26:18 +0800

    shipping order fixed

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -22,15 +22,6 @@ class Product(models.Model):
         return self.name
 
     @property
-    def shipping(self):
-        shipping = False
-        orderitems = self.orderitem_set.all()
-        for i in orderitems:
-            if i.product.digital == False:
-                shipping = True
-        return shipping
-
-    @property
     def imageURL(self):
         try:
             url = self.image.url
@@ -47,6 +38,15 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
 
     @property
     def get_cart_total(self):
