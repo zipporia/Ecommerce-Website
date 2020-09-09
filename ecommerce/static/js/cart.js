fa30195507e8a656238216b2aa7d9cdb31d300d7
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-09 22:24:39 +0800

    Update cart.js

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -19,6 +19,14 @@ for(var i = 0; i < updateBtns.length; i++){
 
 function addCookieItem(productId, action){
     console.log('Not Logged in..')
+
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }else{
+            cart[productId]['quantity'] += 1;
+        }
+    }
 }
 
 
