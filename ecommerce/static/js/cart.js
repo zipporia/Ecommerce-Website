c578d5c0adaef66d78b8ed15f6ea121716803eac
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-09 22:33:23 +0800

    Update cart.js

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -27,6 +27,13 @@ function addCookieItem(productId, action){
             cart[productId]['quantity'] += 1;
         }
     }
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1;
+        if(cart[productId]['quantity'] <= 0){
+            console.log('remove item')
+            delete cart[productId]
+        }
+    }
 }
 
 
