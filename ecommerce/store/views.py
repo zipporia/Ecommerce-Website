852a446b7c22cc5b11c09e8f5962e12ca3b5f42f
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-10 17:26:15 +0800

    view update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -37,6 +37,15 @@ def cart(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]["quantity"])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]["quantity"]
+
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
