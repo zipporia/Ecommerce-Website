4b4f610f776ae0e681a58cb9dda8a170555aa89b
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-10 19:38:27 +0800

    views store update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -14,9 +14,8 @@ def store(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
 
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
@@ -52,7 +51,7 @@ def checkout(request):
         cartItems = cookieData['cartItems']
         order = cookieData['order']
         items = cookieData['items']
-        
+
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
