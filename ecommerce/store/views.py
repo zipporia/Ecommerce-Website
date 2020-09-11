37d9bacaafc38c863a921fe8ba9f366da81aa997
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-11 15:39:00 +0800

    view update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -75,22 +75,8 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
 
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
 
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
     else:
         print('User is not logged in')
 
@@ -120,5 +106,22 @@ def processOrder(request):
                 order=order,
                 quantity=item['quantity']
             )
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
 
     return JsonResponse('Payment complete', safe=False)
