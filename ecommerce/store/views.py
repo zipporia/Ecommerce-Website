60df76ea21e055dead8d84af317040ecbfbb4c8c
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-12 12:50:40 +0800

    guestorder update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -77,34 +77,7 @@ def processOrder(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
     else:
-        print('User is not logged in')
 
-        print('COOKIES:', request.COOKIES)
-        name = data['form']['name']
-        email = data['form']['email']
-
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create(
-            email=email,
-        )
-        customer.name = name
-        customer.save()
-
-        order = Order.objects.create(
-            customer=customer,
-            complete=False,
-        )
-
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-
-            orderItem = OrderItem.objects.create(
-                product=product,
-                order=order,
-                quantity=item['quantity']
-            )
 
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
